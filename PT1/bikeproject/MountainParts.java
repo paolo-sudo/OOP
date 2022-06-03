2c0cb956f09f50ded1d2a417519237434e1554ab
paolo-sudo <paolodaquioag@gmail.com>
2022-06-03 20:35:35 +0800

    PT1 Activity Folder

---
+++ PT1/bikeproject/MountainParts.java
@@ -0,0 +1,13 @@
+package PT1.bikeproject;
+
+public interface MountainParts {
+	//constant declaration
+	public final String TERRAIN = "off road";
+
+	//required methods after implementation
+	public String getSuspension();
+	public void setSuspension(String newValue);
+	public String getType();
+	public void setType(String newValue);
+}//end interface MountainParts
+
---
+++ PT1/bikeproject/RoadParts.java
@@ -0,0 +1,12 @@
+package PT1.bikeproject;
+
+public interface RoadParts {
+	//constant declaration
+	public final String TERRAIN = "track racing";
+
+	//required methods after implementation
+	public int getTyreWidth();
+	public void setTyreWidth(int newValue);
+	public int getPostHeight();
+	public void setPostHeight(int newValue);
+}//end interface RoadParts
