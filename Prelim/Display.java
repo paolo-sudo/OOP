12cf8dd539c37fc0a919416a1f5c70d67014c62f
paolo-sudo <paolodaquioag@gmail.com>
2022-06-14 23:34:15 +0800

    Updated Prelim File

--- Prelim/Display.java
+++ Prelim/Display.java
@@ -10,14 +10,56 @@ import java.util.*;
 public class Display {
     public static void main(String[] args) { 
         Deck deck = new Deck();
-        for (int suit = Card.DIAMONDS; suit <= Card.SPADES; suit++) { 
+   
+        System.out.println();
+        for (int suit = Card.DIAMONDS; suit <= Card.DIAMONDS; suit++) { 
             for (int rank = Card.ACE; rank <= Card.KING; rank++) {
                 Card card = deck.getCard(suit, rank); 
-                System.out.format("\t%s of %s%n",
+                System.out.format("\t%s of %s%n", 
                     card.rankToString(card.getRank()),
                     card.suitToString(card.getSuit()));
             }
         }
+        System.out.println();
+        for (int suit = Card.CLUBS; suit <= Card.CLUBS; suit++) { 
+            for (int rank = Card.ACE; rank <= Card.KING; rank++) {
+                Card card = deck.getCard(suit, rank); 
+                System.out.format("\t%s of %s%n", 
+                    card.rankToString(card.getRank()),
+                    card.suitToString(card.getSuit()));
+            }
+        }
+        System.out.println();
+        for (int suit = Card.HEARTS; suit <= Card.HEARTS; suit++) { 
+            for (int rank = Card.ACE; rank <= Card.KING; rank++) {
+                Card card = deck.getCard(suit, rank); 
+                System.out.format("\t%s of %s%n", 
+                    card.rankToString(card.getRank()),
+                    card.suitToString(card.getSuit()));
+            }
+        }
+        System.out.println();
+        for (int suit = Card.SPADES; suit <= Card.SPADES; suit++) { 
+            for (int rank = Card.ACE; rank <= Card.KING; rank++) {
+                Card card = deck.getCard(suit, rank); 
+                System.out.format("\t%s of %s%n", 
+                    card.rankToString(card.getRank()),
+                    card.suitToString(card.getSuit()));
+            }
+        }
+
+             
+        /* 
+        for (int suit = Card.DIAMONDS; suit <= Card.SPADES; suit++) { 
+            for (int rank = Card.ACE; rank <= Card.KING; rank++) {
+                Card card = deck.getCard(suit, rank); 
+                System.out.format("\t%s of %s%n", 
+                    card.rankToString(card.getRank()),
+                    card.suitToString(card.getSuit()));
+            }
+        }
+        */
+
     }
         
 }
